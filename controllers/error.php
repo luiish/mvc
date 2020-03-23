c244b4d37442dc2026cde824b4896ff94dcf4d4a
luis <luiish.97@mail.com>
2020-03-23 10:33:37 -0600

    archivo mvc

---
+++ controllers/error.php
@@ -0,0 +1,7 @@
+<?php
+class Error{
+    function __construct(){
+        echo "<p>Error al cargar recurso</p>";
+    }
+}
+?>
---
+++ controllers/main.php
@@ -0,0 +1,9 @@
+<?php
+class Main extends Controller{
+    function __construct(){
+        parent ::__construct();
+        echo "<p> Nuevo controlador Main </>";
+    } 
+    
+}
+?>
